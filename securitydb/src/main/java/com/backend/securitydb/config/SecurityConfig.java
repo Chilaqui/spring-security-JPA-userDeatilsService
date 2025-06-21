c13b6e038dc49fe2cd52ab157e55017469e6e0f1
Chilaqui <comhector_@hotmail.com>
2025-06-21 02:59:27 -0600

    Configuracion de el AuthenticationManager

--- securitydb/src/main/java/com/backend/securitydb/config/SecurityConfig.java
+++ securitydb/src/main/java/com/backend/securitydb/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import java.net.http.HttpClient;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,19 +16,24 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())// Deshabilitar CSRF para simplificar la configuración
-            .authorizeRequests( auth -> auth
-                .requestMatchers("/auth/**").permitAll() // Rutas Publicas
-                .anyRequest().authenticated() // Requerir autenticación para cualquier otra solicitud
-            )
-            .formLogin(withDefaults()); // Habilitar el formulario de inicio de sesión
-        return http.build(); 
-            
+                .csrf(csrf -> csrf.disable())// Deshabilitar CSRF para simplificar la configuración
+                .authorizeRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll() // Rutas Publicas
+                        .anyRequest().authenticated() // Requerir autenticación para cualquier otra solicitud
+                )
+                .formLogin(withDefaults()); // Habilitar el formulario de inicio de sesión
+        return http.build();
+
     }
 
-    
+    @Bean
+    // Este método proporciona un AuthenticationManager que se utiliza para autenticar usuarios.
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+
+        return config.getAuthenticationManager();
+
+    }
 }
