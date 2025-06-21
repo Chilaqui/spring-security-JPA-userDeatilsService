3ec32e86c1660de69a7be13a2320c829016c116b
Chilaqui <comhector_@hotmail.com>
2025-06-21 00:55:34 -0600

    Creando laentidad user

---
+++ securitydb/src/main/java/com/backend/securitydb/model/User.java
@@ -0,0 +1,52 @@
+package com.backend.securitydb.model;
+
+import javax.management.relation.Role;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    // Default constructor
+    public User() {
+    }
+
+    //Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUserName() {
+        return userName;
+    }
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Role getRole() {
+        return role;
+    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
