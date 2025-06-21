47cdba2a09389924e56f5298ce2d9429d2c2fff5
Chilaqui <comhector_@hotmail.com>
2025-06-21 03:03:34 -0600

    Integrando PasswordEncoder

--- securitydb/src/main/java/com/backend/securitydb/config/SecurityConfig.java
+++ securitydb/src/main/java/com/backend/securitydb/config/SecurityConfig.java
@@ -4,10 +4,13 @@ import java.net.http.HttpClient;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.method.P;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 // Este archivo configura la seguridad de la aplicación utilizando Spring Security.
@@ -36,4 +39,11 @@ public class SecurityConfig {
         return config.getAuthenticationManager();
 
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(); // Utiliza BCrypt para codificar las contraseñasS
+    }
+
+
 }
