cf722c066b45e9b251228b654d43054308c2f1fd
Chilaqui <comhector_@hotmail.com>
2025-06-21 01:44:37 -0600

    Creando  el metodo optional para buscar un usario

--- securitydb/src/main/java/com/backend/securitydb/repository/UserRepository.java
+++ securitydb/src/main/java/com/backend/securitydb/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.backend.securitydb.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.backend.securitydb.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     
-
+    Optional<User> findByUserName(String userName);
+    // Este método permite buscar un usuario por su nombre de usuario.
+    // Si el usuario existe, devuelve un Optional que contiene el usuario.
 }
