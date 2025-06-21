de708ecb4de9c36a3b3dd5ce22e4106a9b4f2a62
Chilaqui <comhector_@hotmail.com>
2025-06-21 03:28:19 -0600

    Configurando el DaoAuthenticationProvider

--- securitydb/src/main/java/com/backend/securitydb/config/SecurityConfig.java
+++ securitydb/src/main/java/com/backend/securitydb/config/SecurityConfig.java
@@ -1,17 +1,21 @@
 package com.backend.securitydb.config;
 
-import java.net.http.HttpClient;
+
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.method.P;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.backend.securitydb.service.CustomUserDetailsService;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 // Este archivo configura la seguridad de la aplicación utilizando Spring Security.
 
@@ -43,6 +47,15 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder(); // Utiliza BCrypt para codificar las contraseñasS
+    }
+
+    // Este método configura un DaoAuthenticationProvider que utiliza un UserDetailsService personalizado
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(CustomUserDetailsService userDetailsService) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService); // Establece el servicio de detalles del usuario
+        authProvider.setPasswordEncoder(passwordEncoder()); // Establece el codificador de contraseñas
+        return authProvider; // Devuelve el proveedor de autenticación configurado
     }
 
 
