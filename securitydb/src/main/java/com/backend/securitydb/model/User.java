d9b51e5ba6e0847f6fe10a679d13b20348877119
Chilaqui <comhector_@hotmail.com>
2025-06-21 02:16:18 -0600

    Correcion de importacion incorrecta

--- securitydb/src/main/java/com/backend/securitydb/model/User.java
+++ securitydb/src/main/java/com/backend/securitydb/model/User.java
@@ -1,7 +1,5 @@
 package com.backend.securitydb.model;
 
-import javax.management.relation.Role;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
