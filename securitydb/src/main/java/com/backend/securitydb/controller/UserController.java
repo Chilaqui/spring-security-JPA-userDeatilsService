0d9cadc51871429d181112328e1b4c9cad350f6d
Chilaqui <comhector_@hotmail.com>
2025-06-22 02:50:59 -0600

    Eliminamos la logica de negocio de controller para hacer un service de todo los login y register , al final solo importamos el UserService

--- securitydb/src/main/java/com/backend/securitydb/controller/UserController.java
+++ securitydb/src/main/java/com/backend/securitydb/controller/UserController.java
@@ -1,31 +1,34 @@
 package com.backend.securitydb.controller;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.backend.securitydb.model.User;
-import com.backend.securitydb.repository.UserRepository;
+import com.backend.securitydb.service.UserService;
+
+
 
 @RestController
 @RequestMapping("/auth")
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> registerUser(@RequestBody User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
-        return ResponseEntity.ok("User registered successfully!");
+    public ResponseEntity<String> registerUser( @RequestBody User user) {
+        return userService.registerUser(user);
+     }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        return ResponseEntity.ok(userService.login(user));
     }
+
 }
 
