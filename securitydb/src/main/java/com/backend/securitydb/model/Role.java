0a29d3000284309a23f58b890ba04ed24b18ff37
Hector <comhector_@hotmail.com>
2025-06-24 17:30:25 -0600

    Modificamos el rol para que cada rol tenga persmisos de algo

--- securitydb/src/main/java/com/backend/securitydb/model/Role.java
+++ securitydb/src/main/java/com/backend/securitydb/model/Role.java
@@ -1,10 +1,25 @@
 package com.backend.securitydb.model;
 
+import java.util.Set;
+
 public enum Role {
-    ADMIN,
-    DEVELOPER,
-    TESTER,
-    MANAGER,
-    SECURITY_ANALYST,
-    USER
+    ADMIN(Set.of(Permission.READ, Permission.WRITE, Permission.UPDATE)),
+    DEVELOPER(Set.of(Permission.DELETE, Permission.READ, Permission.UPDATE,Permission.WRITE)),
+    TESTER(Set.of(Permission.WRITE, Permission.READ)),
+    MANAGER(Set.of(Permission.WRITE, Permission.READ)),
+    SECURITY_ANALYST(Set.of(Permission.UPDATE, Permission.WRITE, Permission.READ)),
+    USER(Set.of(Permission.READ, Permission.WRITE));
+
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+    public Set<Permission> getPermissions(){
+        return permissions;
+    }
+
+
+
 }
