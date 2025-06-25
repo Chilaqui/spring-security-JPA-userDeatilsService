46e6526f15a13154c4ec5a8d934488d521947567
Hector <comhector_@hotmail.com>
2025-06-24 20:18:37 -0600

    Creamos varias controlles para que un usuario DEVELOPER, pueda crear nuevos usuarios y entradas a mas end-points

--- securitydb/src/main/java/com/backend/securitydb/controller/UserController.java
+++ securitydb/src/main/java/com/backend/securitydb/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.backend.securitydb.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.backend.securitydb.model.User;
 import com.backend.securitydb.service.UserService;
 
-
-
 @RestController
 @RequestMapping("/auth")
 public class UserController {
@@ -20,15 +20,22 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    /* @Secured("DEVELOPER") */
+    @PreAuthorize("hasAuthority('PERM_CREATE')")
     @PostMapping("/register")
-    public ResponseEntity<String> registerUser( @RequestBody User user) {
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
         return userService.registerUser(user);
-     }
+    }
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody User user) {
         return ResponseEntity.ok(userService.login(user));
     }
 
-}
+    /* @PreAuthorize("hasRole('DEVELOPER')") */
+    @GetMapping("/dev-only")
+    public ResponseEntity<String> devOnlyEndpoint() {
+        return ResponseEntity.ok("¡Hola Developer!");
+    }
 
+}
